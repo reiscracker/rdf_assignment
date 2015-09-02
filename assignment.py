d8df0970a1372fd835ec0d9da165c6953cfb53c7
Simon A <simonh.arnold@gmail.com>
2015-09-02 15:31:21 +0200

    Relaunch of the music ontology project

--- assignment.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'reiscracker'
-# coding: utf-8
-from modulescrape import scraper, scraper
-
-scraper.read_modules('2012-09-28_AI-B_Modulhandbuch.txt')
-# Scraper.read_modules('2012-09-28_AI-B_Modulhandbuch.txt')
--- convert_pdf_to_text.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This is a simple bash script with one line to not forget the command used to convert pdf tables to txt
-pdftotext -layout 2012-09-28_AI-B_Modulhandbuch.pdf 2012-09-28_AI-B_Modulhandbuch.txt
